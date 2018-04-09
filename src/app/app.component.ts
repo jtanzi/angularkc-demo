8345bc208bf82ad39ecff37bf5bf46f08eb48cb1
Joel Tanzi <jtanziredux@gmail.com>
2018-04-08 21:45:03 -0500

    Initial buildout of dashboard.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'AngularKC PrimeNg Demo';
+  myName = 'Joel Tanzi';
+  modes = [ 
+    { label: 'Data View', value: 'dataview' },
+    { label: 'Drag & Drop', value: 'dragdrop' }
+  ];
+  selectedMode: 'dataview' | 'dragdrop' = 'dataview';
+  widgetCount = 0;
+
+  widgets = [
+    { name: 'Thingy', count: 6000, category: 'IoT' },
+    { name: 'Pog', count: 500, category: 'Retro' },
+    { name: 'UberThingy', count: 2500, category: 'IoT' },
+    { name: 'Everlast', count: 1000, category: 'Misc' },
+    { name: 'ProTik', count: 1200, category: 'Misc' },
+    { name: 'Lighty', count: 300, category: 'Retro' },
+    { name: 'El Kapitan', count: 500, category: 'IoT' },
+    { name: 'MindBlast', count: 3500, category: 'Misc' },
+    { name: 'ProTik Prime', count: 3100, category: 'Misc' }
+  ];
+
+  constructor( ) {
+    this.widgets.forEach((widget) => {
+      this.widgetCount += widget.count;
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PanelModule } from 'primeng/panel';
 import { TabViewModule } from 'primeng/tabview';
 import { DataViewModule } from 'primeng/dataview';
 import { ChartModule } from 'primeng/chart';
 import { DragDropModule } from 'primeng/dragdrop';
 import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { AppComponent } from './app.component';
 
 
@@ -15,12 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     PanelModule,
+    SelectButtonModule,
     TabViewModule,
     DataViewModule,
     ChartModule,
     DragDropModule,
-    DropdownModule
+    DropdownModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
